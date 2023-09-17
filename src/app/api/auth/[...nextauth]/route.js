d32fbf394dc34c2654fd62c433ac5114b34748e2
Harsh <auharshitsingh@gmail.com>
2023-09-17 12:37:16 +1000

    handling all the login auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
+import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const authOptions = {
@@ -8,4 +9,35 @@ const authOptions = {
       clientSecret: "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",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account.provider === "google") {
+        const { name, email } = user;
+        try {
+          await connectToDb();
+          const isUserExists = await User.findOne({ email });
+
+          if (!isUserExists) {
+            const res = await fetch("/api/user", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ name, email }),
+            });
+            if (res.success) {
+              return user;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      return user;
+    },
+  },
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
